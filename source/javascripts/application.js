7b85475b80f59ccfefeb4969c03ebaf1b338d4c3
Dillon Morton <dillon.morton@imulus.com>
2015-03-02 17:26:19 -0700

    :older_man: WHAT YEAR IS IT?

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,14 +1,18 @@
 //= require jquery
 //= require bootstrap
+//= require _jquery.fittext
 
 (function() {
     var $window = $(window);
     var $hero = $('.hero');
     var $navbar = $('.navbar');
     var viewport = $window.height();
+    var scrollTop = $(window).scrollTop();
 
     $(function() {
         $hero.height(viewport - 30);
+        $('.js-full-height').height(viewport);
+        $('.about-content').height((viewport / 2) - 30 + ($('.bio').height() / 2));
     });
 
     $('a').click(function(){
@@ -21,7 +25,7 @@
     $.fn.detectSection = function(){
             
         var screen = {
-            top : $window.scrollTop(),
+            top : $(window).scrollTop(),
         };
 
         screen.bottom = screen.top + (viewport / 2);
@@ -44,4 +48,6 @@
             };
         });
     });
+
+    $('.js-fittext').fitText(1.2, { minFontSize: '45px' });
 }).call(this);
